1e2a423d85f1a4bd6c07d496cdbdcc87c36194ef
Emelie Skörvald <emelieskorvald@Emelies-MacBook-Pro.local>
2020-08-31 10:32:56 +0200

    ..

--- Week1/Longcounter.java
+++ Week1/Longcounter.java
@@ -4,7 +4,9 @@ package Week1;
  * InnerLongcounter
  */
 public class Longcounter {
-    private long count = 0; public void increment() {
+    private long count = 0; 
+    
+    public void increment() {
         count = count + 1; 
     }
         public synchronized long get() {
