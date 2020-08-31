c38808c1d87e8c83591b95a7bb06230ad1a10606
Emelie Skörvald <emelieskorvald@Emelies-MacBook-Pro.local>
2020-08-31 16:56:56 +0200

    ..

---
+++ Week1/Printer.java
@@ -0,0 +1,31 @@
+package Week1;
+
+public class Printer {
+        public void print() {
+           System.out.print("-");
+           try { Thread.sleep(50); 
+           } catch (InterruptedException exn) {      
+           }
+            System.out.print("|");
+   } 
+
+   public static void main(String[] args) {
+       var p = new Printer();
+       int i = 5;
+
+       Thread t1 = new Thread(() -> {
+        while(i < 10)
+            p.print();
+        
+        });
+         Thread t2 = new Thread(() -> {
+             while(i < 10) 
+                p.print();
+         });
+            t1.start(); t2.start();
+            try { t1.join(); t2.join(); }
+            catch (InterruptedException exn) { 
+            System.out.println("Some thread was interrupted");
+            }
+}
+}
