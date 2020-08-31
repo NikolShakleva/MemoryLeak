194d6cb7af8604cd3abb7b5e67be689c7a37554e
Emelie Skörvald <emelieskorvald@Emelies-MacBook-Pro.local>
2020-08-31 20:24:10 +0200

    ...

--- Week1/Printer.java
+++ Week1/Printer.java
@@ -1,32 +1,34 @@
 
 public class Printer {
 
-        public void print() {
-            synchronized(this) {
-           System.out.print("-");
-           try { Thread.sleep(50); 
-           } catch (InterruptedException exn) {      
-           }
-            System.out.print("|");}
+    public void print() {
+        synchronized(this) {
+            System.out.print("-");
+            try { 
+                Thread.sleep(50); 
+            } catch (InterruptedException exn) {}
+            System.out.print("|");
+        }
    } 
 
-   public static void main(String[] args) {
-       var p = new Printer();
-       int i = 5;
+    public static void main(String[] args) {
+        var p = new Printer();
+        int i = 5;
 
-       Thread t1 = new Thread(() -> {
-        while(i < 10)
-            p.print();
-        
-        });
-         Thread t2 = new Thread(() -> {
-             while(i < 10) 
+        Thread t1 = new Thread(() -> {
+            while(i < 10)
                 p.print();
-         });
-            t1.start(); t2.start();
-            try { t1.join(); t2.join(); }
-            catch (InterruptedException exn) { 
+        });
+
+        Thread t2 = new Thread(() -> {
+            while(i < 10) 
+                p.print();
+        });
+            
+        t1.start(); t2.start();
+        try { t1.join(); t2.join(); }
+        catch (InterruptedException exn) { 
             System.out.println("Some thread was interrupted");
-            }
+        }
 }
 }
