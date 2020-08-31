54a6dc03e709fa5d07bcfcfec4a7e14fd29acaef
Emelie Skörvald <emelieskorvald@Emelies-MacBook-Pro.local>
2020-08-31 20:17:44 +0200

    ..

--- Week1/Longcounter.java
+++ Week1/Longcounter.java
@@ -11,4 +11,23 @@ public class Longcounter {
         public synchronized long get() {
             return count;
         } 
+
+    public static void main(String[] args) {
+        final Longcounter lc = new Longcounter();
+        final int counts = 10_000_000;
+        Thread t1 = new Thread(() -> {
+        for (int i=0; i<counts; i++) 
+            lc.increment();
+        });
+        Thread t2 = new Thread(() -> {
+        for (int i=0; i<counts; i++) 
+           lc.increment();
+        });
+        t1.start(); t2.start();
+        try { t1.join(); t2.join(); }
+        catch (InterruptedException exn) { 
+        System.out.println("Some thread was interrupted");
+        }
+        System.out.println("Count is " + lc.get() + " and should be " + 2*counts);            
+    }
 }
