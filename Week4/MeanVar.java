4149a52a78f270d2493fde31e78b1e6eddd4d644
emsk <emsk@itu.dk>
2020-09-23 13:52:09 +0200

    sdev

--- Week4/MeanVar.java
+++ Week4/MeanVar.java
@@ -5,20 +5,23 @@ public class MeanVar {
 
     public static void main(String[] args) {
         double[] a =
-            //{ 30.7, 30.3, 30.1, 30.7, 50.2, 30.4, 30.9, 30.3, 30.5, 30.8 };
-            {30.7, 100.2, 30.1, 30.7, 20.2, 30.4, 2, 30.3, 30.5, 5.4};
+            { 30.7, 30.3, 30.1, 30.7, 50.2, 30.4, 30.9, 30.3, 30.5, 30.8 };
+            //{30.7, 100.2, 30.1, 30.7, 20.2, 30.4, 2, 30.3, 30.5, 5.4};
         double n = a.length;
         double sum = 0.0; 
         double sumSq = 0.0; 
+        double sst = 0.0;
         for (int i= 0; i < n; i++) { 
             sum += a[i];
+            sst += a[i] * a[i];
         }
         double mean = sum / n;
 
         for (int i= 0; i < n; i++) { 
             sumSq += (a[i] - mean) * (a[i] - mean);
         }        
+        double sdev = Math.sqrt((sst - mean*mean*n)/(n-1));
         double variance = sumSq / n;
-        System.out.printf("%6.1f ns +/- %6.3f %n ", mean, variance); 
+        System.out.printf("%6.1f ns +/- %6.3f %6.3f%n ", mean, sdev, variance); 
     }
 }
